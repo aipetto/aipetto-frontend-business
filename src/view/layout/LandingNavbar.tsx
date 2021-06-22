333f3d1b68a6577521fa952d4469a92bf16c519c
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-06-22 14:53:25 -0300

    Check the correct business route on link condition

--- src/view/layout/LandingNavbar.tsx
+++ src/view/layout/LandingNavbar.tsx
@@ -1,8 +1,6 @@
 /*eslint-disable*/
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-// components
-import IndexDropdown from "../../components/DropDowns/IndexDropdown";
 import {i18n} from "../../i18n";
 import I18nFlags from "./I18nFlags";
 
@@ -31,7 +29,7 @@ export default function LandingNavbar(props) {
                                     <a className="text-gray-900 px-3 py-2 bg-yellow-500 hover:bg-yellow-600 rounded border border-yellow-600 shadow font-semibold transition duration-500 ease-in-out cursor-pointer" href="/beta">Beta</a>
                                 </li>
                                 {
-                                 history.location.pathname == '/business-list' ?
+                                 history.location.pathname == '/business' ?
                                      <li className="nav-item px-2 signup">
                                          <a className="text-gray-900 px-3 py-2 bg-yellow-500 hover:bg-yellow-600 rounded border border-yellow-600 shadow font-semibold transition duration-500 ease-in-out cursor-pointer" href="/">{i18n('public.menu.petsSurvey')}</a>
                                      </li>
