d33418547d5b947caf8b224f7a0503765f8aa9ed
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-10-06 12:49:47 -0300

    Update business dasboard adding reservations info and be operate progress pipeline

--- src/view/dashboard/DashboardPage.tsx
+++ src/view/dashboard/DashboardPage.tsx
@@ -78,6 +78,93 @@ const DashboardPage = (props) => {
   return (
       <>
         {displayDashboardGraph()}
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/3 xl:w-1/3 p-6">
+            <Link to="/service-reservation">
+            <div
+                className="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+              <div className="flex flex-row items-center">
+                <div className="flex-shrink pr-4">
+                  <div className="rounded-full p-5 bg-green-600"><i className="fa fa-wallet fa-2x fa-inverse"></i></div>
+                </div>
+                    <div className="flex-1 text-right md:text-center">
+                    <h5 className="font-bold uppercase text-gray-600">Reservas Confirmadas</h5>
+                    <h3 className="font-bold text-3xl"> 35 <span className="text-green-500"><i
+                        className="fas fa-caret-up"></i></span></h3>
+                  </div>
+              </div>
+            </div>
+            </Link>
+          </div>
+          <div className="w-full md:w-1/3 xl:w-1/3 p-6">
+            <Link to="/service-reservation">
+            <div
+                className="bg-gradient-to-b from-yellow-200 to-yellow-100 border-b-4 border-yellow-600 rounded-lg shadow-xl p-5">
+              <div className="flex flex-row items-center">
+                <div className="flex-shrink pr-4">
+                  <div className="rounded-full p-5 bg-yellow-600"><i className="fas fa-user-plus fa-2x fa-inverse"></i>
+                  </div>
+                </div>
+                <div className="flex-1 text-right md:text-center">
+                  <h5 className="font-bold uppercase text-gray-600">Reservas Esperando confirmar</h5>
+                  <h3 className="font-bold text-3xl">2<span className="text-yellow-600"><i
+                      className="fas fa-caret-up"></i></span></h3>
+                </div>
+              </div>
+            </div>
+           </Link>
+          </div>
+          <div className="w-full md:w-1/3 xl:w-1/3 p-6">
+            <Link to="/service-reservation">
+            <div
+                className="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
+              <div className="flex flex-row items-center">
+                <div className="flex-shrink pr-4">
+                  <div className="rounded-full p-5 bg-blue-600"><i className="fas fa-server fa-2x fa-inverse"></i></div>
+                </div>
+                <div className="flex-1 text-right md:text-center">
+                  <h5 className="font-bold uppercase text-gray-600">Pedidos de Taxi Pet</h5>
+                  <h3 className="font-bold text-3xl">4</h3>
+                </div>
+              </div>
+            </div>
+            </Link>
+          </div>
+
+        </div>
+
+        <div className="flex flex-wrap mb-2 items-center justify-between">
+          <div>
+            <span
+                className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-gray-200">
+              Progresso para que seu negócio esteja recebendo reservas na app Aipetto
+            </span>
+          </div>
+          <div className="text-right px-6">
+            <span className="text-lg font-semibold inline-block text-gray-600">
+              50%
+            </span>
+          </div>
+        </div>
+        <div className="overflow-hidden h-6 mb-4 text-xs flex rounded bg-gray-200">
+          <div style={{width: '25%'}}
+               className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-400">
+            Cadastro do negócio
+          </div>
+          <div style={{width: '25%'}}
+               className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-400">
+            Cadastro de serviços e preços
+          </div>
+          <div style={{width: '25%'}}
+               className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-400">
+            Definir Lugares de atendimento
+          </div>
+          <div style={{width: '25%'}}
+               className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-400">
+            Definir Data e horários disponíveis para reserva por serviço
+          </div>
+        </div>
+
         <div className="p-6 grid grid-cols-2 md:grid-cols-6 gap-4">
 
                 {menus
